2af7af5a037f5ddb1b1d2af748cc9e0b58de4a9c
Samuel Curry <samc735@gmail.com>
2019-01-08 16:53:03 -0500

    daily quest route

--- src/routes/points.js
+++ src/routes/points.js
@@ -1,6 +1,7 @@
 const router = new (require('restify-router')).Router();
 const points = require(`../libs/points/points`);
 const {responseHandler, errorHandler} = require(`../libs/responseHandler`);
+const config = require('../../config');
 
 router.put('/discord-id/:discordID', async function(req, res, next){
     try {
@@ -17,6 +18,19 @@ router.put('/user-id/:userID', async function(req, res, next){
         await points.addPointsByUserID(req.params.userID, req.body.points);
         responseHandler(res, {});
     } catch (err){
+        errorHandler(res, err);
+    }
+    next();
+});
+
+router.put('/user-id/:userID/daily', async function(req, res, next){
+    try {
+        let reward = Math.floor(Math.random() * config.daily.max ) + config.daily.min;
+        console.log(reward, req.params.userID)
+        await points.addPointsByUserID(req.params.userID, reward);
+        responseHandler(res, {reward});
+    } catch (err){
+        console.log(err)
         errorHandler(res, err);
     }
     next();

d16055119158470e14bb3faf27abec9f82de227e
Samuel Curry <samc735@gmail.com>
2019-03-15 23:35:13 -0400

    lottery routes

---
+++ src/routes/lottery.js
@@ -0,0 +1,36 @@
+const router = new (require('restify-router')).Router();
+const Lottery = require('../libs/lottery/lottery');
+const { responseHandler, errorHandler } = require('../libs/responseHandler');
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newLottery = await Lottery.create(req.body.lottery);
+    responseHandler(res, { lottery: newLottery });
+  } catch (err) {
+    errorHandler(res, err);
+  }
+  next();
+});
+
+router.get('/user/:userID/active', async (req, res, next) => {
+  try {
+    const activeLotteries = await Lottery.getActiveLotteryForUserByUserID();
+    responseHandler(res, { lottery: activeLotteries });
+  } catch (err) {
+    errorHandler(res, err);
+  }
+  next();
+});
+
+router.put('/:lotteryID:/status', async(req, res, next) => {
+  try {
+    const updatedLottery = await Lottery.setLotteryStatus(req.params.lotteryID, req.params.isDone);
+    responseHandler(res, { lottery: updatedLottery })
+  } catch (err) {
+    errorHandler(res, err);
+  }
+  next();
+});
+
+
+module.exports = router;

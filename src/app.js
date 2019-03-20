a242f152f239beac23952cda921614f404c8ad17
Samuel Curry <currysl@ucmail.uc.edu>
2019-03-20 16:29:28 -0400

    added lottery to routes

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ const user = require('./routes/user');
 const points = require('./routes/points');
 const commands = require('./routes/commands');
 const guild = require('./routes/guild');
+const lottery = require('./routes/lottery');
 const lotteryTickets = require('./routes/lotteryTickets');
 
 server.use(restify.plugins.throttle({
@@ -30,6 +31,7 @@ router.add('/user', user);
 router.add('/points', points);
 router.add('/commands', commands);
 router.add('/guild', guild);
+router.add('/lottery', lottery);
 router.add('/lottery-ticket', lotteryTickets);
 router.applyRoutes(server);
 

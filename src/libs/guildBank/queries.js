abd65ec9e63d55cca69bbc422150e46faa0c664b
Samuel Curry <currysl@ucmail.uc.edu>
2019-03-29 15:03:05 -0400

    initial queries complete

---
+++ src/libs/guildBank/queries.js
@@ -0,0 +1,17 @@
+exports.create = guildBank => ({
+  name: 'create-guild-bank',
+  text: 'INSERT INTO guild_banks(points, guild_id) VALUES ($1, $2)',
+  values: [guildBank.points, guildBank.guildID],
+});
+
+exports.update = (guildBank, rowID) => ({
+  name: 'update-guild-bank',
+  text: 'UPDATE guild_banks SET points = $1, guild_id = $2 WHERE id = $3',
+  values: [guildBank.points, guildBank.guildID, rowID],
+});
+
+exports.getByDiscordGuildID = guildID => ({
+  name: 'get-guild-bank-by-guild-id',
+  text: 'SELECT * FROM guild_banks WHERE guild_id = $1',
+  values: [guildID],
+});

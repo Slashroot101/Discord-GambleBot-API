806193ec007580a01f9086b7ae26367842e6f0bf
Samuel Curry <currysl@ucmail.uc.edu>
2019-03-29 12:14:42 -0400

    some basic query work

--- src/libs/guild/queries.js
+++ src/libs/guild/queries.js
@@ -1,41 +1,23 @@
-exports.create = (guildID, global = false) => ({
+exports.create = guild => ({
   name: 'create-guild',
-  text: 'INSERT INTO guilds(guild_id, join_date, global) VALUES ($1, now(), $2) ON CONFLICT(guild_id) DO NOTHING RETURNING *',
-  values: [guildID, global],
+  text: 'INSERT INTO guilds(guild_id, join_date, global) VALUES($1, $2, $3)',
+  values: [guild.discordGuildID, guild.joinDate, guild.isGlobal],
 });
 
-exports.createGuildBank = guildID => ({
-  name: 'create-guild-bank',
-  text: 'INSERT into guild_banks(points, guild_id) VALUES (0, $1) ON CONFLICT(guild_id) DO NOTHING RETURNING *',
-  values: [guildID],
-});
-
-exports.addToPointsToGuildBank = (guildBankID, amount) => ({
-  name: 'add-points-to-guild-bank',
-  text: 'UPDATE guild_banks SET points = points + $2 FROM guilds WHERE guilds.guild_id = $1 and guilds.id = guild_banks.guild_id',
-  values: [guildBankID, amount],
-});
-
-exports.getGuildBankByGuildID = guildBankID => ({
-  name: 'get-guild-bank-by-guild-id',
-  text: 'SELECT * FROM guild_banks JOIN guilds on guild_banks.guild_id = guilds.id WHERE guilds.guild_id = $1',
-  values: [guildBankID],
-});
-
-exports.getByGuildID = guildID => ({
-  name: 'get-guild-by-guildID',
-  text: 'SELECT * FROM guilds WHERE guild_id = $1',
-  values: [guildID],
-});
-
-exports.getByID = id => ({
+exports.getByRowID = rowID => ({
   name: 'get-guild-by-id',
   text: 'SELECT * FROM guilds WHERE id = $1',
-  values: [id],
+  values: [rowID],
 });
 
-exports.getGlobalGuild = () => ({
-  name: 'get-global-guild',
-  text: 'SELECT * FROM guilds WHERE global = false',
-  values: [],
+exports.getByDiscordGuildID = discordGuildID => ({
+  name: 'get-guild-by-discord-id',
+  text: 'SELECT * FROM guilds WHERE guild_id = $1',
+  values: [discordGuildID],
+});
+
+exports.updateByRowID = (guild, rowID) => ({
+  name: 'update-by-row-id',
+  text: 'UPDATE guilds set guild_id = $1, join_date = $2, global = $3 WHERE id = $4',
+  values: [guild.guildID, guild.joinDate, guild.isGlobal, rowID],
 });
